5923e57cf34370d3d3d49344a4ba12cdf22262da
henboy10 <henrytalacko@gmail.com>
2021-08-04 11:29:40 -0500

    Updated to use MongoDB

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 load_dotenv()
 
 from re import match
-from os import getenv
+from os import getenv, path
 from time import sleep
 from threading import Thread
 from datetime import datetime
@@ -14,10 +14,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from pymongo import MongoClient
+mongo = MongoClient('<mongodb+srv://Henboy:<21jPzi3zFEvClGnv>@cluster0.rlxx6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority>')
+db_counter = mongo.db.counter
+find = db_counter.find_one({'deleted': {'$exists': True}})
+if not find: db_counter.insert_one({'deleted': 0, 'since': datetime.now()})
+find = db_counter.find_one({'deleted': {'$exists': True}})
+
 app = Flask(__name__)
 twitter_link = 'https://twitter.com/rendersbyian'
 
-count, deleted, seconds = 0, 0, 30
+count, deleted, since, seconds = 0, find['deleted'], find['since'].strftime('%A, %B %d %Y'), 30
 CHROMEDRIVER_PATH = fr'{getenv("CHROMEDRIVER_PATH")}/bin/chromedriver'
 
 options = Options()
@@ -26,10 +33,11 @@ options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.binary_location = getenv('GOOGLE_CHROME_BIN')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
-wait, first_time, tweets, last_checked = WebDriverWait(driver, 10), True, 0, None
+wait, first_time, tweets = WebDriverWait(driver, 10), True, 0
+
 
 def background_updater():
-    global first_time, tweets, deleted, last_checked
+    global first_time, tweets, deleted
 
     while True:
         if first_time:
@@ -41,17 +49,18 @@ def background_updater():
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="primaryColumn"] h2[role="heading"] + div[dir="auto"]')))
         check = driver.find_element_by_css_selector('[data-testid="primaryColumn"] h2[role="heading"] + div[dir="auto"]')
         number = int(match(r'[\d,]+', check.text).group().replace(',', ''))
-        if number < tweets: deleted += (tweets - number)
-        last_checked = datetime.now().strftime('%A, %B %d %Y')
-        tweets = number
+        if number < tweets:
+            deleted += (tweets - number)
+            db_counter.find_one_and_update({'deleted': {'$exists': True}}, {'$set': {'deleted': deleted}}, upsert=True)
 
+        tweets = number
         sleep(seconds)
 
 Thread(target=background_updater).start()
 
 @app.route('/')
 def hello():
-    return render_template('index.html', tweets=tweets, deleted=deleted, last_checked=last_checked)
+    return render_template('index.html', tweets=tweets, deleted=deleted, since=since)
 
 PORT = getenv('PORT') or 8080
 if __name__ == '__main__':
